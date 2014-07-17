f0ebdc63818897bec3f1b9973b2c648848b8a916
Lee Whitaker <lwhitaker@brightcove.com>
2014-07-17 14:41:24 -0400

    Update example bc page and commands to create it.

--- commands/pmapi.sh
+++ commands/pmapi.sh
@@ -15,7 +15,7 @@ Create:
               "http://ec2-107-20-72-18.compute-1.amazonaws.com:2005/plugins/videojs-playlist-loader.js",
               "http://ec2-107-20-72-18.compute-1.amazonaws.com:2005/plugins/videojs-playlists.js",
               "http://ec2-107-20-72-18.compute-1.amazonaws.com:2005/plugins/videojs.teleport-plugin.js",
-              "http://ec2-107-20-72-18.compute-1.amazonaws.com:2005/plugins/videojs-overlay.min.js"
+              "http://ec2-107-20-72-18.compute-1.amazonaws.com:2005/plugins/videojs-overlay.min.js",
               "http://ec2-107-20-72-18.compute-1.amazonaws.com:2005/plugins/player.js"
           ],
           "plugins": [{
@@ -38,4 +38,4 @@ curl \
       --header "Content-Type: application/json" \
       --user lee.whitaker@gmail.com \
       --request POST \
-      https://players.api.brightcove.com/v1/accounts/17359650/players/83d5321f-1e36-4ccc-a4bb-220631ff822b/publish
+      https://players.api.brightcove.com/v1/accounts/17359650/players/f4d83778-0b6e-46b9-a3d6-86db2cc08d0a/publish
